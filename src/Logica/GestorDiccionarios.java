2908d6ea29b17c2dbdaea2be5ec02eed07aed357
Michael <mramirezg7@ucenfotec.ac.cr>
2025-12-02 20:09:23 -0600

    Menu y consola separado

---
+++ src/Logica/GestorDiccionarios.java
@@ -0,0 +1,30 @@
+package Logica;
+
+import java.util.ArrayList;
+
+public class GestorDiccionarios {
+
+    private ArrayList<DiccionarioTecnico> tecnicos;
+    private ArrayList<DiccionarioEmocional> emocionales;
+
+    public GestorDiccionarios() {
+        tecnicos = new ArrayList<>();
+        emocionales = new ArrayList<>();
+    }
+
+    public void agregarTecnico(DiccionarioTecnico dt) {
+        tecnicos.add(dt);
+    }
+
+    public void agregarEmocional(DiccionarioEmocional de) {
+        emocionales.add(de);
+    }
+
+    public ArrayList<DiccionarioTecnico> getTecnicos() {
+        return tecnicos;
+    }
+
+    public ArrayList<DiccionarioEmocional> getEmocionales() {
+        return emocionales;
+    }
+}
